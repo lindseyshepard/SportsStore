065909317ce340185c8adcda4dc0fe39b0b81177
lindseyshepard <55165590+lindseyshepard@users.noreply.github.com>
2019-09-25 08:18:53 -0400

    Sports Store Continued
    Continued class work

--- Program.cs
+++ Program.cs
@@ -8,22 +8,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
+
 namespace SportsStore
 {
-   
-        public class Program
-        {
-            public static void Main(string[] args)
-            {
-                BuildWebHost(args).Run();
-            }
-
-            public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-            .UseStartup<Startup>()
-            .UseDefaultServiceProvider(options =>
-            options.ValidateScopes = false)
-            .Build();
-        }
-    
+    public class Program
+    {
+        public static void Main(string[] args) { BuildWebHost(args).Run(); }
+        public static IWebHost BuildWebHost(string[] args) => WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().UseDefaultServiceProvider(options => options.ValidateScopes = false).Build();
+    }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,105 +1,45 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Builder;
-//using Microsoft.AspNetCore.Hosting;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.Extensions.DependencyInjection;
-//using SportsStore.Models;
-//using Microsoft.Extensions.Configuration;
-//using Microsoft.EntityFrameworkCore;
-
-//namespace SportsStore
-//{
-//    public class Startup
-//    {
-
-//        public Startup(IConfiguration configuration) =>
-//            Configuration = configuration;
-//        public IConfiguration Configuration
-//        {
-//            get;
-//        }
-
-
-//        // This method gets called by the runtime. Use this method to add services to the container.
-//        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-//        public void ConfigureServices(IServiceCollection services)
-//        {
-//            //    The ConfigureServices method is used to set up shared objects that 
-//            //    can be used throughout the application through the dependency injection feature
-//            services.AddDbContext<ApplicationDbContext>(options => 
-//            options.UseSqlServer(Configuration["Data:SportStoreProducts:ConnectionString"]));
-//            services.AddTransient<IProductRepository, EFProductRepository>();
-//           // services.AddTransient<IProductRepository, FakeProductRepository>();
-//            services.AddMvc();
-
-
-//        }
-
-//        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-//        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-//        {
-//            app.UseDeveloperExceptionPage(); //This extension method displays details of exceptions that occur in the application, which is useful during the development process. It should not be enabled in deployed application
-//            app.UseStatusCodePages(); //This extension method adds a simple message to HTTP responses that would not otherwise have a body, such as 404 - Not Found responses.
-//            app.UseStaticFiles(); //This extension method enables support for serving static content from the wwwroot folder.
-//            app.UseMvc(routes => { //This extension method enables ASP.NET Core MVC.
-//            });
-
-//            app.UseMvc(routes => {
-//                routes.MapRoute(name: "default", 
-//                    template: "{controller=Product}/{action=List}/{id?}");
-//            });
-
-//            if (env.IsDevelopment())
-//            {
-//                app.UseDeveloperExceptionPage();
-//            }
-
-//            app.Run(async (context) =>
-//            {
-//                await context.Response.WriteAsync("Hello World!");
-//            });
-//        }
-//    }
-//}
-
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SportsStore.Models;
-using Microsoft.Extensions.Configuration;
-using Microsoft.EntityFrameworkCore;
+
+
 namespace SportsStore
 {
     public class Startup
     {
         public Startup(IConfiguration configuration) => Configuration = configuration;
         public IConfiguration Configuration { get; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-        //    services.AddDbContext<ApplicationDbContext>(options => 
-        //    options.UseSqlServer(Configuration["Data:SportStoreProducts:ConnectionString"]));
-        services.AddDbContext<ApplicationDbContext>(options =>   
-            options.UseSqlServer(Configuration["Data:SportStoreProducts:ConnectionString"]));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["Data:SportStoreProducts:ConnectionString"]));
             services.AddTransient<IProductRepository, EFProductRepository>();
+            services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddMvc();
+            services.AddMemoryCache();
+            services.AddSession();
         }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseDeveloperExceptionPage(); app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
             app.UseStaticFiles();
-            app.UseMvc(routes => {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Product}/{action=List}/{id?}");
+            app.UseSession();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(name: null, template: "{category}/Page{productPage:int}", defaults: new { controller = "Product", action = "List" });
+                routes.MapRoute(name: null, template: "Page{productPage:int}", defaults: new { controller = "Product", action = "List", productPage = 1 });
+                routes.MapRoute(name: null, template: "{category}", defaults: new { controller = "Product", action = "List", productPage = 1 });
+                routes.MapRoute(name: null, template: "", defaults: new { controller = "Product", action = "List", productPage = 1 });
+                routes.MapRoute(name: null, template: "{controller}/{action}/{id?}");
             });
             SeedData.EnsurePopulated(app);
         }
